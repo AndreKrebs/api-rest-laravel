3cefa2be6685aa0dddf598e8ed71e39c99804606
André Krebs <andre.krebssilva@gmail.com>
2018-01-03 19:52:49 -0200

    Add arquivos do controller

---
+++ app/Http/Controllers/CompaniesController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Http\Controllers;
+
+use App\Company;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class CompaniesController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/JobsController.php
@@ -0,0 +1,16 @@
+<?php namespace App\Http\Controllers;
+
+use App\Job;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class JobsController extends Controller
+{
+    public function index()
+    {
+        $jobs = Job::with('company')->get();
+        return response()->json($jobs);
+    }
+}
