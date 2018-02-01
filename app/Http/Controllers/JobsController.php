f16686d2ec9babe7db542b4631404fea9d994b35
André Krebs <andre.krebssilva@gmail.com>
2018-02-01 12:30:31 -0200

    Add jwt.auth em Jobs controller

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -10,7 +10,7 @@ class JobsController extends Controller
 {
     
     public function __construct() {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('jwt.auth'); //, ['except' => ['index', 'show']]);
     }
     
     public function index()
